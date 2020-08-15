94257c27cde0c4c66c3008ea29190ec1c3015be9
Yury_Shakhouski <Jeorgeo@list.ru>
2020-08-15 21:36:30 +0300

    task-200815-2136
    механика табов

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,19 @@ $(document).ready(function () {
     $('.js-accordeon-btn').not(this).removeClass('active').next().slideUp();
   });
 
+  $('.js-tab-btn').click(function () {
+    $(this).toggleClass('active').next().slideToggle();
+    $('.js-tab-btn').not(this).removeClass('active').next().slideUp();
+  });
+
+  /**/
+
   let mobileMenu = document.querySelectorAll('.js-mobileMenu');
   let mobileSearch = document.querySelectorAll('.js-mobileSearch');
   let mobileContacts = document.querySelectorAll('.js-mobileContacts');
   let mobileProfile = document.querySelectorAll('.js-mobileProfile');
+  //let flyTab = document.querySelectorAll('.js-fly-tab');
+  //let flyTabBtn = document.querySelectorAll('.js-fly-tab-btn');
 
   function moveTabMenu(item1, item2) {
     item1.classList.toggle('active');
