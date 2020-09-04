e7e05ddabc023907858dd64281228882420b4539
Yury_Shakhouski <Jeorgeo@list.ru>
2020-09-04 18:02:10 +0300

    task-200904-1800
    верстка каталог запчастей - 70%

--- assets/js/script.js
+++ assets/js/script.js
@@ -96,6 +96,32 @@ if (document.querySelector('.js-faq-slider')) {
   });
 }
 
+if (document.querySelector('.js-service-slider')) {
+  $('.js-service-slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        }
+      }
+    ]
+  });
+}
+
 
   /*------------------Slider Слайдер сертификатов--------------*/
 
